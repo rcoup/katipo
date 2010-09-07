2fe61dbdf22f58687102af03f0422289e7728887
craig.ds <craig.ds@6255859c-e1d8-11dd-8fb1-9960f2a117f8>
2010-09-07 22:59:07 +0000

    add MIDDLEWARE CLASSES override (for compatibility with django 1.2 which has a new default CSRF middleware)

--- katipo/settings.py
+++ katipo/settings.py
@@ -14,3 +14,10 @@ INSTALLED_APPS = (
     'katipo',
     'django.contrib.sessions',
 )
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
